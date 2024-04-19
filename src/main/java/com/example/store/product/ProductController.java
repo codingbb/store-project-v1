fc549bc68e9233fcd150cb75eae926800fb56d36
SCHcoding <codingb007@gmail.com>
2024-04-20 00:27:31 +0900

    기본 컨트롤러 생성

--- src/main/java/com/example/store/product/ProductController.java
+++ src/main/java/com/example/store/product/ProductController.java
@@ -2,10 +2,42 @@ package com.example.store.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
 public class ProductController {
     private final ProductService productService;
+
+    @GetMapping("/product")
+    public String list() {
+
+        return "/product/list";
+    }
+
+    @GetMapping("/product/{id}")
+    public String detail(@PathVariable Integer id) {
+
+        return "/product/detail/" + id;
+    }
+
+    @GetMapping("/product/save-form")
+    public String saveForm() {
+
+        return "/product/save-form";
+    }
+
+    @GetMapping("/product/update-form")
+    public String updateForm() {
+
+        return "/product/update-form";
+    }
+
+    @GetMapping("/")
+    public String main() {
+
+        return "/index";
+    }
 
 }
