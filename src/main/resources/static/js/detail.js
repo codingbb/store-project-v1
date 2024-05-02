c13a2a0b578859b30c287aaf09d462c42901846c
SCHcoding <codingb007@gmail.com>
2024-05-02 10:24:04 +0900

    상품금액 표시 에러 수정

--- src/main/resources/static/js/detail.js
+++ src/main/resources/static/js/detail.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
     // 가격 표시
     var price = $('input[name="price"]').val();
-    $('input[name="price"]').val(priceToString(parseInt(price)));
+    // 콤마를 제거한 후 숫자로 변환
+    var numPrice = parseInt(price.replace(/,/g, ''));
+    $('input[name="price"]').val(priceToString(numPrice));
 
     // 수량 표시
     var qty = $('input[name="qty"]').val();
-    $('input[name="qty"]').val(qtyToString(parseInt(qty)));
+    // 콤마를 제거한 후 숫자로 변환
+    var numQty = parseInt(qty.replace(/,/g, ''));
+    $('input[name="qty"]').val(qtyToString(numQty));
 });
 
 function priceToString(price) {
